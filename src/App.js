e790983dda6d8a22cb305bd82332f52c3f41fb83
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-08-18 19:33:10 -0500

    se agrego la seccion About

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css"
 import Menu from "./components/Menu";
 import Hero from "./components/Hero";
-
+import About from "./components/About";
 function App() {
   return (
     <div className="App">
       <Menu />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../assets/css/About.css"
+
+const About = () => {
+    return (
+        <section class="more-about">
+            <h2>Mas acerca de mi</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+                nesciunt excepturi quos obcaecati incidunt voluptatem ipsam sunt ipsum,
+                autem deleniti cupiditate molestias quis unde quae totam porro dicta
+                iure animi inventore, veniam hic! Omnis nulla, delectus a voluptatibus
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur
+                nostrum dolor minus, libero delectus praesentium perferendis
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
+                consequuntur labore? Ea totam voluptas amet!
+            </p>
+        </section>
+    );
+};
+
+export default About;
