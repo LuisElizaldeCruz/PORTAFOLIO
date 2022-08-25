f283465edd358a5cf22b4d89b25db0f330bff91a
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-08-24 23:41:29 -0500

    se agrego el estilo css personalizado al campo de texto multilinea

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider, } from '@mui/material/styles';
 
-const theme = createTheme({
+
+const themetxt = createTheme({
     components: {
         // Name of the component
         MuiTextField: {
@@ -45,54 +46,94 @@ const theme = createTheme({
         },
     },
 });
+
+const theme2 = createTheme({
+    components: {
+        // Name of the component
+        MuiTextField: {
+            styleOverrides: {
+                // Name of the slot
+                root: {
+
+                    '& label.Mui-focused': {
+                        color: 'green',
+                    },
+
+                    '& fieldset': {
+                        borderColor: '#273b91',
+                        borderWidth: 2,
+                    },
+                    '& .MuiOutlinedInput-root':{
+                        '&.Mui-focused fieldset': {
+                            borderColor: 'red !important',
+                        }
+                    }
+                },
+            },
+        },
+    },
+});
+
+
 export const Form = () => {
-
-
     return (
-        <Box
-            component="form"
-            sx={{
-                width: "500px",
-                maxWidth: '100%',
-                margin: '0 auto',
-                padding: '0 10px',
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <div className="form-container">
-                <ThemeProvider theme={theme}>
-                    <TextField
-                        required
-                        id="outlined-required"
-                        fullWidth
-                        label="nombre"
-                        placeholder="tu nombre"
-                        margin="normal"
+        <>
+            <h3 className="form-title">Contactame</h3>
+            <Box
+                component="form"
+                sx={{
+                    width: "500px",
+                    maxWidth: '100%',
+                    margin: '0 auto',
+                    padding: '0 10px',
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <div className="form-container">
+                    <ThemeProvider theme={themetxt}>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            fullWidth
+                            label="Nombre"
+                            placeholder="Tu nombre"
+                            margin="normal"
 
-                    />
-                    <TextField
-                        required
-                        id="outlined-required"
-                        fullWidth
-                        label="correo"
-                        placeholder="tu correo"
-                        margin="normal"
-                    />
-                </ThemeProvider>
-                <TextField
-                    id="outlined-multiline-static"
-                    required
-                    label="tu mensaje"
-                    placeholder="tu mensaje aqui"
-                    multiline
-                    rows={4}
-                    fullWidth
-                    margin="normal"
-                />
+                        />
+                        <TextField
+                            required
+                            id="outlined-required"
+                            fullWidth
+                            label="Asunto"
+                            placeholder="Asunto"
+                            margin="normal"
+                        />
+                        <TextField
+                            required
+                            id="outlined-required"
+                            fullWidth
+                            label="correo"
+                            placeholder="Tu correo"
+                            margin="normal"
+                        />
+                    </ThemeProvider>
+                    <ThemeProvider theme={theme2}>
+                        <TextField
+                            id="outlined-multiline-static"
+                            required
+                            label="tu mensaje"
+                            placeholder="tu mensaje aqui"
+                            multiline
+                            rows={4}
+                            fullWidth
+                            margin="normal"
+                            color="primary"
+                        />
+                    </ThemeProvider>
+                </div>
 
-            </div>
-
-        </Box>
+            </Box>
+        </>
     );
 };
