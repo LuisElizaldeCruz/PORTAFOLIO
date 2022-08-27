52311286c9d503e2a58e8cff7910dc08f5cf66f0
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-08-27 00:43:37 -0500

    se agrego estilos css a los proyectos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
+import '../assets/css/Projects.css'
 
-export default function Project({ img, alt, description, link="#"}) {
+export default function Project({ img, alt, description, link = "#" }) {
   return (
     <>
-    <div className="project-card">
-    <img className="project-img" src={require(`../assets/images-projects/${img}.png`)} alt={alt} />
-    <p className="description-text">{description}</p>
-    </div>
+      <div className="project-card">
+        <img className="project-img" src={require(`../assets/images-projects/${img}.png`)} alt={alt} />
+        <p className="description-text">{description}</p>
+      </div>
     </>
   );
 }
