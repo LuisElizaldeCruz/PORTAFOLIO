40e11c6151d916c8efc91b6e2a26f837f64a35f0
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-09-14 00:50:44 -0500

    se hizo correcciones al diseño responsivo y al texto de la descripcion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,19 +6,12 @@ const About = ({ moreDescription }) => {
         <section className="more-about">
             <h2>Mas acerca de mi</h2>
             <p>
-                Egresado de la carrera de ingenieria en computación enfocado al desarrollo de software, autodidacta aprendiendo ingles y japones, con interes y conocimiento en el uso de <b>linux</b> y el software libre. Actualmente me estoy enfocando en el desarrollo web usando <strong>HTML, CSS, JavaScript y React y en el backend utilizando php y MySQL</strong>.
+                Egresado de la carrera de ingenieria en computación enfocado al desarrollo de software, autodidacta aprendiendo ingles y japones, con interes y conocimiento en el uso de <b>linux</b> y el software libre. Actualmente me estoy enfocando en el desarrollo web usando <strong>HTML, CSS, JAVASCRIPT y REACT y en el backend utilizando PHP y MYSQL</strong>.
             </p>
             <p>
-                Durante mi estancia en la universidad utilicé JAVA para la creacion de aplicaciones lo que me ayudo a entender la programación orientada a objetos y sus caracteristicas como el polimorfismo, herencia, etc.
+                Durante mi estancia en la universidad utilicé <b>JAVA</b> para la creacion de aplicaciones lo que me ayudo a entender la programación orientada a objetos y sus caracteristicas como el polimorfismo, herencia, etc.
             </p>
-            {/* <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur
-                nostrum dolor minus, libero delectus praesentium perferendis
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
-                consequuntur labore? Ea totam voluptas amet!
-            </p> */}
+            
         </section>
     );
 };
