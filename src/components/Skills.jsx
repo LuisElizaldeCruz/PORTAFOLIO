e2edad787b55438fa329dc780a0801a70178c3c6
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-09-23 23:56:24 -0500

    se corrigio el contenedor del set de skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,25 +8,21 @@ import sassico from "../assets/icons/icons8-sass.svg"
 import reactico from "../assets/icons/icons8-react.svg"
 import githubico from "../assets/icons/icons8-github.svg"
 
-
-
-
-
-const Skills=()=>{
-    return(
-        <section className="skills" id="skills">
-          <h2 className="skill-header">Skills</h2>
-          <div className="skills-set animate__animated animate__pulse">
-            <img src={htmlico} alt="icono-html" loading="lazy" className="icon icon-card" />
-            <img src={css3ico} alt="icono-cc3" loading="lazy" className="icon icon-card" />
-            <img src={jsico} alt="icono-js" loading="lazy" className="icon icon-card" />
-            <img src={gitico} alt="icono-git" loading="lazy" className="icon icon-card" />
-            <img src={githubico} alt="icono-git" loading="lazy" className="icon icon-card" />
-            <img src={sassico} alt="icono-git" loading="lazy" className="icon icon-card" />
-            <img src={reactico} alt="icono-git" loading="lazy" className="icon icon-card" />
-          </div>
-        </section>
-    );
+const Skills = () => {
+  return (
+    <section className="skills" id="skills">
+      <h2 className="skill-header">Skills</h2>
+      <div className="skills-set animate__animated animate__pulse">
+        <div className="icon-card"><img src={htmlico} alt="icono-html" loading="lazy" className="icon" /></div>
+        <div className="icon-card"><img src={css3ico} alt="icono-cc3" loading="lazy" className="icon" /></div>
+        <div className="icon-card"><img src={jsico} alt="icono-js" loading="lazy" className="icon" /></div>
+        <div className="icon-card"><img src={gitico} alt="icono-git" loading="lazy" className="icon" /></div>
+        <div className="icon-card"><img src={githubico} alt="icono-git" loading="lazy" className="icon" /></div>
+        <div className="icon-card"><img src={sassico} alt="icono-git" loading="lazy" className="icon" /></div>
+        <div className="icon-card"><img src={reactico} alt="icono-git" loading="lazy" className="icon" /></div>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
