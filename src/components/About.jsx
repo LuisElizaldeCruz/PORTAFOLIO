28e3f315c2ea789df8b62f8200f8d0c6b65de139
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-09-26 00:55:56 -0500

    se corrigio el tamaño de la cabecera y la posicion del menu responsivo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,9 @@ import "../assets/css/About.css"
 const About = ({ moreDescription }) => {
     return (
         <section className="more-about">
-            <h2>Mas acerca de mi</h2>
+            <h2>Acerca de mi</h2>
             <p>
-                Egresado de la carrera de ingenieria en computación enfocado al desarrollo de software, autodidacta aprendiendo ingles y japones, con interes y conocimiento en el uso de <b>linux</b> y el software libre. Actualmente me estoy enfocando en el desarrollo web usando <strong>HTML, CSS, JAVASCRIPT y REACT y en el backend utilizando PHP y MYSQL</strong>.
+                Egresado de la carrera de ingenieria en computación, con interes y conocimiento en el uso de <b>linux</b> y el software libre. Actualmente me estoy enfocando al desarrollo web usando <strong>HTML, CSS, JAVASCRIPT y REACT y en el backend utilizando PHP y MYSQL</strong>.
             </p>
             <p>
                 Durante mi estancia en la universidad utilicé <b>JAVA</b> para la creacion de aplicaciones lo que me ayudo a entender la programación orientada a objetos y sus caracteristicas como el polimorfismo, herencia, etc.
