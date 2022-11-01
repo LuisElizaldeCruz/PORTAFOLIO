23265b9e82db676d1a08249a8da26750e4836798
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-10-31 23:04:57 -0600

    se agrego el efecto lazy loading al cargar las imagenes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,7 +5,7 @@ export default function Project({ projectTitle, img, alt, description, link = "#
   return (
     <>
       <div className="project-card contenedor-img">
-        <img className="project-img" src={require(`../assets/imgProjects/${img}`)} alt={alt} />
+        <img className="project-img" src={require(`../assets/imgProjects/${img}`)} alt={alt} loading="lazy"/>
         <div className="mascara">
           <h2>{projectTitle}</h2>
           <p className="description-text">{description}</p>
