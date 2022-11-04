0ae3af5e529216b6223565eda6a73e73788205a4
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-11-03 23:29:22 -0600

    se cambio el estilo hover de la seccion de proyectos

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,7 +7,7 @@ export default function Project({ projectTitle, img, alt, description, link = "#
       <div className="project-card contenedor-img">
         <img className="project-img" src={require(`../assets/imgProjects/${img}`)} alt={alt} loading="lazy"/>
         <div className="mascara">
-          <h2>{projectTitle}</h2>
+          <h4 className="project-title">{projectTitle}</h4>
           <p className="description-text">{description}</p>
           <a href={link} className="link">Ver demo</a>
         </div>
