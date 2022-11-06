fe1c444d73980f61a198ddf25991bdf37066f42d
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-11-06 00:46:55 -0600

    se agrego los recursos para la seccion estudio

--- src/components/Estudios.jsx
+++ src/components/Estudios.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import "../assets/css/Estudios.css";
+import iconUaemex from "../assets/multimedia/uaemex.png"
 
 export default function Estudios() {
     return (
         <section className="estudios">
             <h4 className="estudios-titulo">Estudios</h4>
             <div className="licenciatura">
-                <div className="img-escuela">img</div>
+            <img className="img-escuela" src={iconUaemex} alt="uaemex"/>
                 <h4 className="nombre-lic">Ingenieria en computacion</h4>
                 <p className="nombre-escuela">Centro Universitario UAEM Atlacomulco</p>
+                <p className="fecha">2015-2020</p>
             </div>
         </section>
     )
